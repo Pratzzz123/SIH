e1a7c1e9bc57e22c3dde9ddc6fa77a14c4efbddf
Pratzz123 <prathameshind305@gmail.com>
2023-09-20 03:37:49 +0530

    some changes

--- cyberRakshak/cyberRakshak/info.py
+++
@@ -1,5 +0,0 @@
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtop.gmail.com'
-EMAIL_HOST_USER = 'smarturjaproject@gmail.com'
-EMAIL_HOST_PASSWORD = 'wbml bkfz rhwr xdlz'
-EMAIL_PORT = 587
--- cyberRakshak/cyberRakshak/settings.py
+++ cyberRakshak/cyberRakshak/settings.py
@@ -11,12 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from .info import *
-EMAIL_USE_TLS = EMAIL_USE_TLS
-EMAIL_HOST = EMAIL_HOST
-EMAIL_HOST_USER = EMAIL_HOST_USER
-EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
-EMAIL_PORT = EMAIL_PORT
+from .passw import PWD
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -131,6 +127,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'smarturjaproject@gmail.com'
-EMAIL_HOST_PASSWORD = 'yoomiqhvxgtijijk'
+EMAIL_HOST_PASSWORD = PWD
 EMAIL_PORT = 587
 EMAIL_USE_TLS=True
