c52f0115f16d7f7037caf461d8c9239750ee2b8b
Pratzz123 <prathameshind305@gmail.com>
2023-09-20 03:41:50 +0530

    debug: false

--- cyberRakshak/cyberRakshak/settings.py
+++ cyberRakshak/cyberRakshak/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3zismg4$$#bwpf-cme#ga8#*$v^rejdzmuk#+&1z$y+fm(b=h1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
